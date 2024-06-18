2f7735ffb5485cf305a83405334ef49acbef70fc
Bruno <br902823@gmail.com>
2024-06-18 17:25:20 -0300

    cul_ds

---
+++ cul_ds.py
@@ -0,0 +1,17 @@
+import openpyxl as xl
+import datetime  
+
+RA = input('digite o seu RA: ')
+livro = input('digite o livro que deseja retirar: ')
+categoria = input('digite a categoria do livro: ')
+data = datetime.date.today()
+data_entrega_temp = 0
+data_entrega_temp.append(data.replace()) 
+data_entrega = data_entrega_temp
+responsavel = input('digite o nome do responsavel: ')
+
+
+print(data)
+print(data_entrega)
+
+
